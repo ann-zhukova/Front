e38e1b15ecd225cb1a6c64a70b9b1c7943511aa8
Gorby <MegaRs1@yandex.ru>
2024-11-24 23:58:15 +0400

    fix

--- Nedugov/front/scripts/add_steps.js
+++
@@ -1,7 +0,0 @@
-function addStep() {
-        const stepsContainer = document.getElementById('stepsContainer');
-        const newStep = document.createElement('input');
-        newStep.rows = 2;
-        newStep.placeholder = "Следующий шаг";
-        stepsContainer.appendChild(newStep);
-}
